fad234fc8bdabe53ee29fceba51634ee231c5804
abhishekmauryaKsolves <abhishekmaurya@ksolves.com>
2026-09-02 17:11:16 +0530

    pushing hello word dag

---
+++ dags/hello_world_dag.py
@@ -0,0 +1,20 @@
+from airflow import DAG
+from airflow.providers.standard.operators.python import PythonOperator
+from datetime import datetime
+
+
+def hello_world():
+    print("Hello World")
+
+
+with DAG(
+    dag_id="hello_world_dag",
+    start_date=datetime(2026, 9, 1),
+    schedule=None,
+    catchup=False,
+) as dag:
+
+    hello = PythonOperator(
+        task_id="hello_world",
+        python_callable=hello_world,
+    )
